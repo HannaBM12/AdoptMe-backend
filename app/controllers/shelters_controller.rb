26421c0347d94403b8828231b46aa5c7ae4bfb40
Jabair <jabairk@gmail.com>
2021-04-20 16:11:12 -0400

    added shelter info

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -6,5 +6,11 @@ class SheltersController < ApplicationController
         render json: shelters, except: [:created_at, :updated_at]
     end
 
+    def show
+        shelter = Shelter.find_by(id: params[:id])
+
+        render json: shelter, except: [:created_at, :updated_at]
+    end
+
 
 end
--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,5 +1,5 @@
 class PetSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image, :age, :breed, :bio, :is_adopted, :shelter_name
+  attributes :id, :name, :image, :age, :breed, :bio, :is_adopted, :shelter_name, :shelter_id
 
   def shelter_name
     self.object.shelter.name
