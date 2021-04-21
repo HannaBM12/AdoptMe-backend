3eac38cb5c9608e2738aa165c6075defec2eb31a
Jabair <jabairk@gmail.com>
2021-04-21 18:50:27 -0400

    working on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,25 @@
 class CommentsController < ApplicationController
-    # def index
-    #     comments = Comment.all
-    #     render json: comments, except:[:created_at, :updated_at]
+    def index
+        comments = Comment.all
+        render json: comments, except:[:created_at, :updated_at]
 
-    # end
+    end
+
+    def create
+        new_comment = Comment.create!(comment_params)
+
+        render json: new_comment
+
+    end
+
+
+    private
+
+    def comment_params
+        # byebug 
+        params.permit(:owner_id, :shelter_id, :message)
+        ### owner_id, pet_id
+
+    end
+
 end
